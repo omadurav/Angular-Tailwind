64b313cf3d39f70426c9eac313c49defbbb8aa68
omadurav <omadurav@outlook.com>
2023-09-27 17:10:43 -0500

    Creacion del proyecto angular integrando tailwind

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DbzComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    DbzComponent
+  ],
+  exports:[
+    DbzComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
